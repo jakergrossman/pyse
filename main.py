a345aa1898c59eb03f595f0083ea632a0916007d
jakergrossman <jake.r.grossman@gmail.com>
2020-09-30 00:32:32 -0500

    Implement the DictTree class
    Implement a dictionary tree, or DictTree class. Each node of the tree is
    either a direct mapping to a value (leaf node) or a mapping to a
    dictionary sub-tree. Example:
    
        dict_tree = {
            "leaf": "leaf_value",
            "dict": {
                "dict_key1": "dict_key1_value",
                "dict_key2": "dict_key2_value"
            }
        }

--- main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-from stackexchange import query_questions
-
-print(query_questions('vi', tagged=["vim", "autocmd"]))
--- stackexchange/structures.py
+++ stackexchange/structures.py
@@ -1,16 +1,45 @@
-class LookupDict(dict):
-    """Dictionary lookup object"""
+"""
+stackexchange.models
+~~~~~~~~~~~~~~~~~~~~
 
-    def __init__(self, name=None):
-        self.name = name
-        super(LookupDict, self).__init__()
+This module contains the structures used in the Stack Exchange API wrapper
+
+:copyright: (c) 2020 by Jake Grossman
+:license: MIT, see LICENSE for more details.
+"""
+# FIXME: Needs tests
+class DictTree(dict):
+    """
+    Dictionary tree, where each node is either a direct mapping to a value
+    (leaf node), or a dictionary sub-tree (internal node).
+    """
+    def __init__(self, dictionary=None, name=None):
+        # FIXME: Needs docstring
+        # internal variable used for __repr__
+        self._name = name
+        if dictionary is not None:
+            for key, value in {k: v for k, v in dictionary.items() if k is not None}.items():
+                if isinstance(value, dict):
+                    # recursively initialize internal dict
+                    setattr(self, key, DictTree(dictionary=value, name=name + '/' + key))
+                else:
+                    # this is a leaf node of the dictionary,
+                    setattr(self, key.upper(), value)
+        super(DictTree, self).__init__()
+
 
     def __repr__(self):
-        return f"<lookup '{self.name}'>"
+        return f"<lookup '{self._name}'>"
 
     def __getitem__(self, key):
-        # allow fallthrough, defualt to None
+        # allow fallthrough, default to None
         return self.__dict__.get(key, None)
 
     def get(self, key, default=None):
         return self.__dict__.get(key, default)
+
+    def keys(self):
+        # FIXME: Needs docstring
+        # filter out all keys starting with an underscore, those
+        # are not directly exposed to the user
+        return {k: v for k,v in self.__dict__.items() if not k.startswith("_")}.keys()
