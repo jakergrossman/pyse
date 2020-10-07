7f13f009538bc78a66ce0172b97e00290bd1fa31
jakergrossman <jake.r.grossman@gmail.com>
2020-10-07 10:29:30 -0500

    First tests

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-tests = $(wildcard test/*.py)
+tests = $(wildcard test/*.py) $(wildcard test/*/*.py)
 
 test:
-	python3 -m unittest $(tests)
+	python3 -m unittest -v $(tests)
 
 .PHONY: test
--- test/test_create_filter.py
+++ test/test_create_filter.py
@@ -1,22 +1,27 @@
 import unittest
 
-from stackexchange import create_filter, base_filters
+from pyse import create_filter, base_filters
 
-class TestFilter(unittest.TestCase):
+class TestDefaultFilters(unittest.TestCase):
     def test_filter_no_args(self):
-        self.assertEqual(create_filter(), "default")
+        default_filter = create_filter()
+        self.assertEqual(default_filter, "default")
 
     def test_filter_base_default(self):
-        self.assertEqual(create_filter(base_filters.DEFAULT), "default")
+        default_filter = create_filter(base=base_filters.DEFAULT)
+        self.assertEqual(default_filter, "default")
 
     def test_filter_base_withbody(self):
-        self.assertEqual(create_filter(base_filters.WITHBODY), "withbody")
+        withbody_filter = create_filter(base=base_filters.WITHBODY)
+        self.assertEqual(withbody_filter, "!7tRYRBs6sfcsYU38wnbid8.F5yky8jpmxi")
 
     def test_filter_base_none(self):
-        self.assertEqual(create_filter(base_filters.NONE), "none")
+        none_filter = create_filter(base=base_filters.NONE)
+        self.assertEqual(none_filter, "none")
 
     def test_filter_base_total(self):
-        self.assertEqual(create_filter(base_filters.TOTAL), "total")
+        total_filter = create_filter(base=base_filters.TOTAL)
+        self.assertEqual(total_filter, "total")
 
 if __name__ == "__main__":
     unittest.main()
